39ffbdee8cb4e39af30408994e825cbb7aa716d0
malignantz <Garrett.Holmes@gmail.com>
2020-04-06 22:27:41 -0600

    added loading spinner for missions

--- src/app/page/MissionsVolunteered/Missions.jsx
+++ src/app/page/MissionsVolunteered/Missions.jsx
@@ -3,7 +3,7 @@ import { useFirestore, firestoreConnect } from "react-redux-firebase";
 import { useSelector, connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 
-import { Typography, Button, Grid } from "@material-ui/core";
+import { Typography, Button, Grid, CircularProgress } from "@material-ui/core";
 import styled from "styled-components";
 
 import { Page, Card } from "../../layout";
@@ -31,12 +31,17 @@ const MissionsPage = ({ auth, history, firebase, ...rest }) => {
     User.assignAsVolunteer(firestore, missionId, auth.uid);
   }
 
-  if (!missions) {
-    return <div> isloading...</div>;
+  if (missions === undefined) {
+    return (
+      <Grid container justify="center" alignItems="center" style={{ minHeight: "100vh" }}>
+        <CircularProgress />
+      </Grid>
+    );
   }
   return (
     <Page template="pink">
       <StyledHeader variant="h1"> Missions </StyledHeader>
+      {Array.isArray(missions) && missions.length === 0 ? <div>No missions</div> : ""}
 
       {missions.map((mission) => (
         <Card key={mission.id}>
